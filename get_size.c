0e0853f41457180fcc38e112131472ec6a656848
dsimwinga <infor.dsimwinga@gmail.com>
2023-09-18 12:06:41 +0200

    adding files

--- get_size.c
+++ get_size.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+*get_size - Calculates the size to cast the argument
+*@format: Formatted string in which to print the arguments
+*@i: List of arguments to be printed.
+*Return: Precision.
+*/
+
+int get_size(const char *format, int *i)
+{
+	int curr_i = *t + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*t = curr_i - 1;
+	else
+		*t = curr_i;
+
+	return (size);
+}
